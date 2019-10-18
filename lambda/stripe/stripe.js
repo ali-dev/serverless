8e287e9dd9632189c89cad2b531124cf9f69a429
Ali <ali@homerlearning.com>
2019-10-17 23:10:33 -0400

    Added appsync stripe configuration. Added addCharge stripe lambda function

--- lambda/stripe/stripe.js
+++ lambda/stripe/stripe.js
@@ -1,14 +1,31 @@
 'use strict';
 
+const stripe = require('stripe')('sk_test_9O0fNrOk3IZwt05uCZMAkgRd00ZiywEHGO'); 
 
 module.exports.createCharge = (event, context, callback) => {
-  return callback(null, {
-                statusCode: 200,
-                headers: {
-                  "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
-                  "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
-                },
-                body: JSON.stringify({success:'true'})
-            });
+  
+	(async () => {
+	  const session = await stripe.checkout.sessions.create({
+	    payment_method_types: ['card'],
+	    line_items: [{
+	      name: 'T-shirt',
+	      description: 'Comfortable cotton t-shirt',
+	      images: ['https://example.com/t-shirt.png'],
+	      amount: 500,
+	      currency: 'usd',
+	      quantity: 1,
+	    }],
+	    success_url: 'https://example.com/success?session_id={CHECKOUT_SESSION_ID}',
+	    cancel_url: 'https://example.com/cancel',
+	  });
+	})();
+  	return callback(null, {
+	        statusCode: 200,
+	        headers: {
+	          "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
+	          "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
+	        },
+	        body: JSON.stringify({success:'true'})
+        });
 
 };
