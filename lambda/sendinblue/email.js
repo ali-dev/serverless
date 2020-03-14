9da42a453e7271fdf225422fb1f99713cdc45e64
Ali <ali@homerlearning.com>
2020-03-14 18:51:34 -0400

    Adds event date validation

--- lambda/sendinblue/email.js
+++ lambda/sendinblue/email.js
@@ -2,16 +2,45 @@
 
 const postmark = require("postmark")
 
+// @todo figure out why this cannot be called
+const invitationValidation = function(data) {
+  let validation = {
+    isValid: true,
+    message: null
+  }
+  if (!data.eventDate) {
+    validation.isValid = false;
+    validation.message = 'Event Date is not specified';
+  }
+  return validation;
+}
 const sendInvitation = (event, context, callback) => {
-  console.log(`EVENT IS: ${JSON.stringify(event)}`);
   const data = JSON.parse(event.arguments.data);
 
 
   const serverToken = process.env.POSTMAN_SERVER_TOKEN; //"xxxx-xxxxx-xxxx-xxxxx-xxxxxx";
   const client = new postmark.ServerClient(serverToken);
   
+  // let errorMessage = null;
+  // const invitationValidation = invitationValidation(data);
+  
+  if (!data.eventDate) {
+    return callback(null, {
+      statusCode: 400,
+      headers: {
+        "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+        "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS
+      },
+      body: JSON.stringify({ error: "true", "message":  'Event date not specified'})
+    });
+    
+  }
+
   
   (async () => {
+    if (!data.eventDate) {
+      throw 'Event Date should be specified';
+    } 
     // trigger email here
     client.sendEmailWithTemplate({
       "From": "ali@causeandcuisine.com", //@todo determine if it is best practice to add the sender's email 
